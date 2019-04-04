56960546ab1b9d659c33b6b0c7f0016842aa73c9
Debdeep Basu <debdeepbasucal@hotmail.com>
2019-04-03 20:34:17 -0400

    Handle bitfield and send interested message.

--- src/bittorrent/MessageHandler.java
+++ src/bittorrent/MessageHandler.java
@@ -18,11 +18,13 @@ import java.util.logging.Logger;
 public class MessageHandler extends Thread {
     
     private HandshakeObject handshakeObject = null;
+    private PeerHandler peerHandler = null;
     Logger log;
     
-    MessageHandler(HandshakeObject handshakeObject) {
+    MessageHandler(PeerHandler peerHandler, HandshakeObject handshakeObject) {
         this.handshakeObject = handshakeObject;
         this.log = GlobalConstants.log;
+        this.peerHandler = peerHandler;
     }
     
     
@@ -39,6 +41,8 @@ public class MessageHandler extends Thread {
                     
                 } else if (message.getMessageType() == GlobalConstants.messageType.UNCHOKE.getValue()) {
                     
+                } else if (message.getMessageType() == GlobalConstants.messageType.INTERESTED.getValue()) {
+                    handleInterestedMessage(message);
                 }
             }
         }
@@ -46,8 +50,47 @@ public class MessageHandler extends Thread {
     
     private void handleBitfieldMessage(ActualMessage message) {
         System.out.print("bitfield message received" + message.getLength());
+        // TODO why do we have to set the peer's bitfield again, even when it was was set during handshake?
         GlobalConstants.PEERLIST.get(handshakeObject.getPeerID()).setChunks(BitSet.valueOf(message.getMessage()));
+        
         log.info("received bitfield message from peer"+handshakeObject.getPeerID()+"--"+GlobalConstants.PEERLIST.get(handshakeObject.getPeerID()).getChunks());
+        
+        // send currentPeer's bitfield to peer if it's not empty
+        if (!Peer.currentPeer.getChunks().isEmpty()) {
+            ActualMessage responseBitfieldMessage = new ActualMessage();
+            responseBitfieldMessage.setMessageType(GlobalConstants.messageType.BITFIELD.getValue());
+            responseBitfieldMessage.setMessage(Peer.currentPeer.getChunks().toByteArray());
+            responseBitfieldMessage.setLength(responseBitfieldMessage.getMessage().length+1);
+            peerHandler.sendMessage(responseBitfieldMessage);
+        }
+        
+        // TODO: find out if you don't have a chunk which your peer has
+        // a:  0011
+        // b:  1011
+        // a & b == a means a is interested in b (provided a is not equal to b)
+        
+        BitSet a = Peer.currentPeer.getChunks();
+        BitSet b = GlobalConstants.PEERLIST.get(handshakeObject.getPeerID()).getChunks();
+        
+        Boolean interested = false;
+        
+        if (!a.equals(b)) {
+            a.and(b);
+            interested = a.equals(Peer.currentPeer.getChunks());
+        }
+        
+        if (interested) {
+            log.info("current peer port: "+Peer.currentPeer.getHostPort()+"is interested!");
+            ActualMessage interestedMessage = new ActualMessage();
+            interestedMessage.setMessageType(GlobalConstants.messageType.INTERESTED.getValue());
+            interestedMessage.setLength(1);
+            peerHandler.sendMessage(interestedMessage);
+        }
+     
         log.info("current peer port: "+Peer.currentPeer.getHostPort()+"my bitfield:"+Peer.currentPeer.getChunks());
+    }
+    
+    private void handleInterestedMessage(ActualMessage message) {
+        log.info("received interested message from"+Integer.toString(handshakeObject.getPeerID())+",current peer port: "+Peer.currentPeer.getHostPort());
     }
 }
--- src/bittorrent/beans/GlobalConstants.java
+++ src/bittorrent/beans/GlobalConstants.java
@@ -33,7 +33,13 @@ public class GlobalConstants {
     public static enum messageType {
     CHOKE((byte)0),
     UNCHOKE((byte)1),
-    BITFIELD((byte)5);
+    INTERESTED((byte)2),
+    NOT_INTERESTED((byte)3),
+    HAVE((byte)4),
+    BITFIELD((byte)5),
+    REQUEST((byte)6),
+    PIECE((byte)7);
+    
     private final byte value;
 
     private messageType(byte val)
